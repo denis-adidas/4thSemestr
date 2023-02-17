ccf4a593277a4dbd75f50e30b35a9b4024d9bd6d
denis_adidas <veb-kamera2013@ya.ru>
2023-02-17 15:20:00 +0300

    Create Main.java

---
+++ lab2/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        matrix a = new matrix(2);
+        a.setMat(1, 1, 0);
+        matrix b = a;
+        System.out.println(a.toStr());
+
+
+        for (int i = 1; i < 10; i++) {
+            a = a.product(b);
+            System.out.println(a.toStr());
+        }
+
+
+    }
+}
