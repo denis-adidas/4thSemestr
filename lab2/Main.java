a189f73de35d4800623ee69d4f62b4cde6192897
denis_adidas <veb-kamera2013@ya.ru>
2023-02-21 21:15:52 +0300

    Update Main.java

--- lab2/Main.java
+++ lab2/Main.java
@@ -1,17 +1,26 @@
 public class Main {
     public static void main(String[] args) {
 
-        matrix a = new matrix(2);
-        a.setMat(1, 1, 0);
-        matrix b = a;
+        matrix a = new matrix(3);
+        a.setMat(0, 1, 3);
+
+        matrix b = new matrix(3);
+        b.setMat(0, 1, 3);
+
         System.out.println(a.toString());
 
 
-        for (int i = 1; i < 10; i++) {
-            a = a.product(b);
-            System.out.println(a.toString());
-        }
+//        for (int i = 1; i < 10; i++) {
+//            a = a.product(b);
+//            System.out.println(a.toString());
+//        }
+        a.doMiracle(1);
+        System.out.println(a.toString());
+
+        b.doMiracle(0);
+        System.out.println(b.toString());
 
 
     }
 }
+//отобраение отнсоительно главной и побочной
