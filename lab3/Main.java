682d7a0a591ed976b103f667ffc9bee4b2bcc53d
denis_adidas <veb-kamera2013@ya.ru>
2023-02-27 18:38:00 +0300

    Update Main.java

--- lab3/Main.java
+++ lab3/Main.java
@@ -1,15 +1,17 @@
+import Matrixes.*;
 public class Main {
     public static void main(String[] args) {
 
         Matrix a = new Matrix(2, 2);
-        Matrix b = new Matrix(3, 2);
+        Matrix b = new Matrix(2, 2);
 
-        a.setMat(1, 1, 1);
+//        a.setMat(1, 1, 1);
 
         a = a.sum(b);
 
+        SquareMatrix c = new SquareMatrix(6);
 
-        System.out.println(a.toString());
+        System.out.println(c.toString());
 
 
     }
