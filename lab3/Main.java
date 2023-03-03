2d45cd8463faae2b597c971d1688ad25f9cab25b
denis-adidas <veb-kamera2013@ya.ru>
2023-03-03 21:45:38 +0300

    Update Main.java

--- lab3/Main.java
+++ lab3/Main.java
@@ -2,15 +2,15 @@ import Matrixes.*;
 public class Main {
     public static void main(String[] args) {
 
-        Matrix a = new Matrix(2, 2);
-        Matrix b = new Matrix(2, 2);
+        Matrix a = new Matrix(2, 3);
+//        Matrix b = new Matrix(2, 2);
+        a.setMat(1, 1, 1);
+        FourPartMatrix c = new FourPartMatrix(a);
 
-//        a.setMat(1, 1, 1);
+//        a = a.sum(b);
 
-        a = a.sum(b);
-
-        SquareMatrix c = new SquareMatrix(6);
-
+//        SquareMatrix c = new SquareMatrix(6);
+        System.out.println(a.toString());
         System.out.println(c.toString());
 
 
