0a2d99ff7cfa9c61d39f6d36760eb88fe3b23d33
denis-adidas <veb-kamera2013@ya.ru>
2023-03-04 20:45:27 +0300

    Update FourPartMatrix.java

--- lab3/Matrixes/FourPartMatrix.java
+++ lab3/Matrixes/FourPartMatrix.java
@@ -1,28 +1,33 @@
 package Matrixes;
 
 public class FourPartMatrix extends Matrix {
+  int extra_row;
+  int extra_col;
+
 public FourPartMatrix(Matrix a) {
   super(a.row, a.col);
+
+  this.extra_row = a.row;
+  this.extra_col = a.col;
+
   this.row = a.row * 2;
   this.col = a.col * 2;
 
-  mat = new int[a.row * 2][a.col * 2];
-
-  for (int i = 0; i < a.row; i++)
-    for (int j = 0; j < a.col; j++) {
-      mat[i][j] =  a.getValue(i,j);
-    }
-  for (int i = 0; i < a.row; i++)
-    for (int j = 0; j < a.col; j++) {
-      mat[i][j + a.col] =  a.getValue(i,j);
-    }
-  for (int i = 0; i < a.row; i++)
-    for (int j = 0; j < a.col; j++) {
-      mat[a.row + i][j] =  a.getValue(i,j);
-    }
-  for (int i = 0; i < a.row; i++)
-    for (int j = 0; j < a.col; j++) {
-      mat[a.row + i][a.col + j] =  a.getValue(i,j);
-    }
+  this.mat = a.mat.clone();
 }
+  public int getValue(int r, int c) {
+    r = r % extra_row;
+    c = c % extra_col;
+    if (((r >= 0) && (r <= row)) && ((c >= 0) && (c <= col)))
+      return mat[r][c];
+    else
+      throw new MatErrors("Out of bound");
+  }
+  public void setMat(int r, int c, int value) {
+    if (((r >= 0) && (r <= row)) && ((c >= 0) && (c <= col))) {
+      mat[r][c] = value;
+    }
+    else
+      throw new MatErrors("Out of bound");
+  }
 }
