f924a3732ad2a88364ac29eb3c7f1082ad6c6d41
denis-adidas <veb-kamera2013@ya.ru>
2023-03-07 11:34:11 +0300

    Update Main.java

--- lab4/Main.java
+++ lab4/Main.java
@@ -1,5 +1,18 @@
+import SortedIntegerList.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SortedIntegerList a = new SortedIntegerList(false);
+        SortedIntegerList b = new SortedIntegerList(false);
+        b = a;
+        System.out.println(a.equal(b));
+        a.add(5);
+        a.add(1);
+        a.add(5);
+        a.add(2);
+        a.remove(5);
+        System.out.println(a.toString());
+        System.out.println(b.toString());
+
+
     }
 }
