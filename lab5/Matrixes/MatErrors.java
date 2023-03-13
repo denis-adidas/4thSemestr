8819fc79440ad7c020f8908b1514a2582fb9f987
denis_adidas <45899852+denis-adidas@users.noreply.github.com>
2023-03-13 15:54:40 +0300

    Create MatErrors.java

---
+++ lab5/Matrixes/MatErrors.java
@@ -0,0 +1,10 @@
+package Matrixes;
+public class MatErrors extends RuntimeException{
+    public MatErrors(String message){
+        super(message);
+    }
+    @Override
+    public String getMessage(){
+        return super.getMessage();
+    }
+}
