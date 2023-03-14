4cce53daaa0a01d108e45e010170442a07ac1d73
denis_adidas <45899852+denis-adidas@users.noreply.github.com>
2023-03-14 19:12:16 +0300

    Update SparseMatrix.java

--- lab5/Matrixes/SparseMatrix.java
+++ lab5/Matrixes/SparseMatrix.java
@@ -4,18 +4,31 @@ import java.util.LinkedList;
 import java.util.ListIterator;
 
 public class SparseMatrix implements Matrix {
-
+    LinkedList<Node> [] table;
     int row;
     int col;
-    LinkedList<Node> [] table;
 
     SparseMatrix(int row, int col) {
+        this.row = row;
+        this.col = col;
 
+        table = new LinkedList[row];
+        for (int i = 0; i < row; i++)
+            table[i] = new LinkedList<Node>();
     }
 
     @Override
     public Matrix add(Matrix a) throws SumMatrixException {
-        return null;
+        if ((row != a.row) || (col != a.col)) {
+            throw new MatErrors("Matrix's size are differents");
+        }
+        Matrix temp = new UsualMatrix(row, col);
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                temp.setValue(i, j, this.getValue(i, j) + a.getValue(i, j));
+            }
+        }
+        return temp;
     }
 
     @Override
@@ -25,12 +38,30 @@ public class SparseMatrix implements Matrix {
 
     @Override
     public int getValue(int row, int col) {
+        if ((row > this.row) || (col > this.col))
+            throw new MatErrors("Unable to execute: out of bound");
+        for (Node it : table[row]) {
+            if (it.getIndex() == col)
+                return it.getValue();
+        }
         return 0;
     }
 
     @Override
     public void setValue(int row, int col, int value) {
-
+        if ((row > this.row) || (col > this.col))
+            throw new MatErrors("Unable to execute: out of bound");
+        if (getValue(row, col) != 0) {
+            for (Node it : table[row]) {
+                if (it.getIndex() == col) {
+                    it.setValue(value);
+                }
+            }
+        }
+        else {
+            Node a = new Node(col, value);
+            table[row].add(a);
+        }
     }
 
     @Override
