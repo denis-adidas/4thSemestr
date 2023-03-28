aad70647f376414221a3ac659abc97f3651c706b
denis_adidas <45899852+denis-adidas@users.noreply.github.com>
2023-03-28 21:21:35 +0300

    Update Main.java

--- lab4/Main.java
+++ lab4/Main.java
@@ -3,13 +3,13 @@ public class Main {
     public static void main(String[] args) {
         SortedIntegerList a = new SortedIntegerList(false);
         SortedIntegerList b = new SortedIntegerList(false);
-        System.out.println(a.equal(b));
+//        System.out.println(a.equal(b));
         a.add(5);
         a.add(1);
         a.add(5);
         a.add(6);
         a.add(2);
-        b = a.getLessThan(5);
+        b = a.getLessThan(-1);
 //        a.remove(5);
         System.out.println(a.toString());
         System.out.println(b.toString());
