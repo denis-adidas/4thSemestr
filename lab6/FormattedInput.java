9bfd7fa525b81a878ae310b23f946fcc27aab06d
denis-adidas <veb-kamera2013@ya.ru>
2023-04-02 00:41:28 +0300

    Update FormattedInput.java

--- lab6/FormattedInput.java
+++ lab6/FormattedInput.java
@@ -1,13 +1,12 @@
-import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Scanner;
 
 public class FormattedInput {
-
     private static final String[] VALID_FORMATS = {"%d", "%f", "%s", "%c"};
+
     public static void main(String[] args) {
-        Object[] test = scanf("%d %s %c");
+        Object[] test = scanf("%d %s");
         for (Object obj : test) {
             System.out.println(obj.getClass().getName() + ": " + obj);
         }
@@ -26,26 +25,23 @@ public class FormattedInput {
             }
         }
     }
-
-    public static Object[] sscanf(String  format, String input) {
-        String[] inputData = input.split(" ");
-        HashSet<String>  validFormats = new HashSet<>(Arrays.asList(VALID_FORMATS));
-        HashSet<String> inputFormat = new HashSet<>(Arrays.asList(format.split(" ")));
+    public static Object[] sscanf(String format, String in) {
+        String[] inputData = in.split(" ");
+        ArrayList<String> inputFormat = new ArrayList<>(Arrays.asList(format.split(" ")));
+        ArrayList<String> validFormats = new ArrayList<>(Arrays.asList(VALID_FORMATS));
 
         boolean formatIsValid = true;
-        for (String f : inputFormat) {
-            formatIsValid = validFormats.contains(f);
-            if (!formatIsValid)
-                break;
-        }
-        if (!formatIsValid || inputData.length != inputFormat.size()) {
-            throw new IllegalArgumentException("Error: not valid format.");
-        }
+        for (String it : inputFormat)
+            if (!validFormats.contains(it))
+                throw new IllegalArgumentException("Error: invalid format.");
+
+        if (inputData.length != inputFormat.size())
+            throw new IllegalArgumentException("Error: input data arguments longer than count of format.");
 
         Object[] result = new Object[inputData.length];
         int i = 0;
-        for (String f : inputFormat) {
-            switch (f) {
+        for (String it : inputFormat) {
+            switch (it) {
                 case "%d" -> result[i] = Integer.parseInt(inputData[i]);
                 case "%f" -> result[i] = Double.parseDouble(inputData[i]);
                 case "%s" -> result[i] = inputData[i];
@@ -62,6 +58,5 @@ public class FormattedInput {
             i++;
         }
         return result;
-    }
-
-}
+            }
+        }
