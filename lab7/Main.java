4a65dab559128f4fd8292954b468ada3b5097d65
denis-adidas <veb-kamera2013@ya.ru>
2023-04-09 15:13:04 +0300

    Update Main.java

--- lab7/Main.java
+++ lab7/Main.java
@@ -3,15 +3,11 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Settings a = new Settings();
-        try {
-            a.loadFromBinaryFile(new File("/Users/denis_adidas/IdeaProjects/lab07/src/out2.bin"));
-            System.out.println(a.toString());
-            a.saveInTextFile(new File("/Users/denis_adidas/IdeaProjects/lab07/src/out.txt"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
 
+//        a.loadFromTextFile("H:\\IntelliJ_projects\\lab07\\src\\in.txt");
+        System.out.println(a.toString());
+//        a.saveInTextFile("H:\\IntelliJ_projects\\lab07\\src\\out.txt");
     }
 }
