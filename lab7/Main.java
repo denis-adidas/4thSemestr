fa52d7938d0ad4f1d205c994f16a148b5bb4a299
denis-adidas <veb-kamera2013@ya.ru>
2023-04-10 09:33:51 +0300

    Update Main.java

--- lab7/Main.java
+++ lab7/Main.java
@@ -4,10 +4,11 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Settings a = new Settings();
 
-//        a.loadFromTextFile("H:\\IntelliJ_projects\\lab07\\src\\in.txt");
-        System.out.println(a.toString());
-//        a.saveInTextFile("H:\\IntelliJ_projects\\lab07\\src\\out.txt");
+        Graph a = new Graph(4);
+//        a.loadFromTextFile("H:\\IntelliJ_projects\\lab07\\src\\in_g.txt");
+        a.loadFromBinaryFile("H:\\IntelliJ_projects\\lab07\\src\\out_g.bin");
+//        System.out.println(a.toString());
+//        a.saveInBinaryFile("H:\\IntelliJ_projects\\lab07\\src\\out_g.bin");
     }
 }
