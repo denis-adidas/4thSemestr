d29c7e420f318d3b91c22839dd7be76dbf0f367c
denis-adidas <veb-kamera2013@ya.ru>
2023-04-11 09:42:42 +0300

    Create SumMatrixException.java

---
+++ lab8/Matrixes/SumMatrixException.java
@@ -0,0 +1,10 @@
+package Matrixes;
+public class SumMatrixException extends MatErrors {
+    public SumMatrixException(String message) {
+        super(message);
+    }
+    @Override
+    public String getMessage(){
+        return super.getMessage();
+    }
+}
