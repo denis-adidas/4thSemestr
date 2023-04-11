6b5f03a4f0c518158bd4b9d3af8083ec466223a0
denis-adidas <veb-kamera2013@ya.ru>
2023-04-11 09:42:38 +0300

    Create ProductMatrixException.java

---
+++ lab8/Matrixes/ProductMatrixException.java
@@ -0,0 +1,10 @@
+package Matrixes;
+public class ProductMatrixException extends MatErrors {
+    public ProductMatrixException(String message) {
+        super(message);
+    }
+    @Override
+    public String getMessage(){
+        return super.getMessage();
+    }
+}
