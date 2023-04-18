14877a404f24ede6c5a9d61383a52d467c2a8ff3
denis_adidas <45899852+denis-adidas@users.noreply.github.com>
2023-04-18 16:20:01 +0300

    Create ThreadUtils.java

---
+++ lab8/Knapsack/ThreadUtils.java
@@ -0,0 +1,20 @@
+package Matrixes.Knapsack;
+
+public class ThreadUtils {
+    public static final int NUM_THREAD = 4;
+
+    public static void runThread(Knapsack knapsack, int [] solutions) {
+        Thread [] threads = new Thread[NUM_THREAD];
+        for (int i = 0; i < NUM_THREAD; i++) {
+            threads[i] = new Thread(new KnapsackThreadSolution(knapsack, solutions, i));
+            threads[i].start();
+        }
+        try {
+            for (int i = 0; i < NUM_THREAD; i++) {
+                threads[i].join();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
