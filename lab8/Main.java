d9db5a712740cffa276f71902527cf58d450fe8e
denis_adidas <45899852+denis-adidas@users.noreply.github.com>
2023-04-18 20:50:28 +0300

    Update Main.java

--- lab8/Main.java
+++ lab8/Main.java
@@ -1,5 +1,10 @@
+import Matrixes.Knapsack.Knapsack;
+import Matrixes.Knapsack.KnapsackTask;
+import Matrixes.Knapsack.ThreadUtils;
 import Matrixes.MeasureTime;
 import Matrixes.UsualMatrix;
+
+import java.security.MessageDigest;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,6 +23,21 @@ public class Main {
         MeasureTime.measureTime("Parallel multiplication", () -> {
             cd.product(a, b);
         }, 1);
-        System.out.println("matrices are equal: " + c.equals(d));
+        System.out.println(cd.toString());
+
+//        int[] weights = {4, 1, 3};
+//        int[] values = {4000, 2000, 2500};
+//        int capacity = 4;
+//        Knapsack knapsack = new Knapsack(weights, values, capacity);
+//        int[] solutions = new int[capacity + 1];
+//        ThreadUtils tu = new ThreadUtils();
+//        MeasureTime.measureTime("Thread solution", () -> {
+//            tu.runThread(knapsack, solutions);
+//        }, 1);
+////        System.out.println("The maximum value that can be put in a knapsack of capacity " + capacity + " is " + solutions[capacity]);
+//
+//        MeasureTime.measureTime("Standart solution", () -> {
+//            KnapsackTask.KnapsackSolution(weights, values, capacity);
+//        }, 1);
     }
 }
