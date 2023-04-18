165f0207b7ad410d0a1f249105477fe62aa0e20f
denis_adidas <45899852+denis-adidas@users.noreply.github.com>
2023-04-18 17:07:43 +0300

    Update KnapsackThreadSolution.java

--- lab8/Knapsack/KnapsackThreadSolution.java
+++ lab8/Knapsack/KnapsackThreadSolution.java
@@ -22,15 +22,5 @@ public class KnapsackThreadSolution implements Runnable {
                 arr[j] = Math.max(prevSolution, newSolution);
             }
         }
-
-    }
-    public static void main(String[] args) {
-        int[] weights = {4, 1, 3};
-        int[] values = {4000, 2000, 2500};
-        int capacity = 4;
-        Knapsack knapsack = new Knapsack(weights, values, capacity);
-        int[] solutions = new int[capacity + 1];
-        ThreadUtils.runThread(knapsack, solutions);
-        System.out.println("The maximum value that can be put in a knapsack of capacity " + capacity + " is " + solutions[capacity]);
     }
 }
